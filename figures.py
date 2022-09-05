d8b76665a9490d8c139d23a03941613f6900076d
Paola Contreras <paolacontreras231@gmail.com>
2022-09-04 21:15:29 -0600

    almost done

---
+++ figures.py
@@ -0,0 +1,56 @@
+import numpy as np
+import math_lib as ml 
+
+WHITE = (1,1,1)
+BLACK = (0,0,0)
+
+#Point of contact 
+class Intersect(object):
+    def __init__(self, distance, point, normal, sceneObj):
+        self.distance = distance
+        self.point = point
+        self.normal = normal
+        self.sceneObj = sceneObj
+
+class Material(object):
+    #diffuse is use only to put some color 
+    def __init__(self, diffuse = WHITE):
+        self.diffuse = diffuse
+
+
+class Sphere(object):
+    def __init__(self, center, radius, material):
+        self.center = center
+        self.radius = radius
+        self.material = material
+
+    def ray_intersect(self, orig, dir):
+        L = ml.subtract(self.center, orig)
+        tca = ml.dot(L, dir)
+        #d_norm = ml.normalized(L)
+        #d= ((ml.power(d_norm,2)) - tca ** 2 )**0.5
+        d = ( np.linalg.norm(L) ** 2 - tca ** 2) ** 0.5
+
+
+        if d > self.radius:
+            return None
+
+        thc = (self.radius ** 2 - d ** 2) ** 0.5
+
+        t0 = tca - thc
+        t1 = tca + thc
+
+        if t0 < 0:
+            t0 = t1
+        if t0 < 0:
+            return None
+        
+        # P = O + t0 * D
+        P = ml.add(orig, t0 * dir)
+        normal = ml.subtract(P, self.center)
+        normal = ml.normalized(normal)
+
+        return Intersect(distance = t0,
+                         point = P,
+                         normal = normal,
+                         sceneObj = self)
---
+++ main.py
@@ -0,0 +1,44 @@
+from gl import Raytracer, V3
+from figures import *
+from light import *
+
+
+width = 900
+height = 900
+
+# Materiales
+
+orange = Material(diffuse = (1,0.5,0))
+black = Material(diffuse = (0, 0, 0))
+white = Material(diffuse = (1,1,1))
+red = Material(diffuse = (1,0,0))
+
+
+rtx = Raytracer(width, height)
+
+rtx.lights.append( AmbientLight( ))
+rtx.lights.append( DirectionalLight(direction = (-1,-1,-1) ))
+
+#Body of the snowman
+rtx.scene.append( Sphere(V3(0,-3,-10), 2, white) )
+rtx.scene.append( Sphere(V3(0,0,-10), 1.5, white) )
+rtx.scene.append( Sphere(V3(0,2.3,-10), 1, white) )
+
+#Buttons 
+rtx.scene.append( Sphere(V3(0,-1.5,-6), 0.30, black) )
+rtx.scene.append( Sphere(V3(0,-0.6,-6), 0.25, black) )
+rtx.scene.append( Sphere(V3(0,0.2,-6), 0.20, black) )
+
+#Face
+rtx.scene.append( Sphere(V3(0,1.7,-7), 0.1, orange) )
+rtx.scene.append( Sphere(V3(0,1.4,-7), 0.08, black) )
+rtx.scene.append( Sphere(V3(0.2,1.43,-7), 0.08, black) )
+rtx.scene.append( Sphere(V3(-0.2,1.43,-7), 0.08, black) )
+rtx.scene.append( Sphere(V3(-0.2,1.9,-7), 0.11, black) )
+rtx.scene.append( Sphere(V3(0.2,1.9,-7), 0.11, black) )
+rtx.scene.append( Sphere(V3(0.16,1.35,-5), 0.02, white) )
+rtx.scene.append( Sphere(V3(-0.16,1.35,-5), 0.02, white) )
+
+rtx.glRender()
+
+rtx.glFinish("output.bmp")
